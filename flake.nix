5ab73f816b4e657bf607d07a9120f1a4c9c4ffc9
Yawen Guan <yawen.guan@proton.me>
2026-01-25 00:26:37 +0100

    Add nix flake support

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A nix-flake-based rocq development environment with iris";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = (
+              with pkgs;
+              [
+                coq
+                coqPackages.iris
+              ]
+            );
+          };
+        }
+      );
+}
